1e84c8b960c1822032957f04988c90f64a85df62
ll835 <loktinlo@gmail.com>
2022-11-25 10:38:43 +0000

    implement local storage functionality
    draft newLocalStorage, updateLocalStorage, and restoreLastSession functions; amend resetAllTiles and revealTile functions to track changes in local storage

--- script.js
+++ script.js
@@ -2,19 +2,54 @@ const input = document.querySelector("input");
 const days = document.querySelectorAll(".day");
 const overlayButtons = document.querySelectorAll(".overlay");
 const resetButton = document.querySelector(".reset-btn");
-const userHistory = {};
+const userHistory = JSON.parse(localStorage.getItem("userHistory")) || newLocalStorage();
 
 function revealTile(tile){
     tile.classList.toggle("revealed");
+
+    const date = tile.parentElement.id;
+    const selectedTile = userHistory.find(element => element.date === date);
+    if (selectedTile.revealed === false) selectedTile.revealed = true;
+    else selectedTile.revealed = false;
+    updateLocalStorage();
 }
 
 function resetAllTiles(){
     overlayButtons.forEach(button => {
-        button.classList.remove("revealed")
+        button.classList.remove("revealed");
+    })
+
+    userHistory.forEach(item => item.revealed = false);
+    updateLocalStorage();
+}
+
+function newLocalStorage(){
+    const localStorage = [];
+    days.forEach(day => {
+        const date = day.id;
+        const revealed = false;
+        const entry = {date, revealed}
+        localStorage.push(entry)
+    })
+    return localStorage
+}
+
+function updateLocalStorage(){
+    localStorage.setItem("userHistory", JSON.stringify(userHistory))
+}
+
+function restoreLastSession(){
+    userHistory.forEach(item => {
+        if (item.revealed){
+            const tileToReveal = document.getElementById(item.date).querySelector(".overlay")
+            tileToReveal.classList.add("revealed")
+        }
     })
 }
+
+document.addEventListener("DOMContentLoaded", restoreLastSession);
 
 overlayButtons.forEach(button => button.addEventListener("click", (event) => {
     revealTile(event.target);
 }))
-resetButton.addEventListener("click", resetAllTiles)
+resetButton.addEventListener("click", resetAllTiles);
